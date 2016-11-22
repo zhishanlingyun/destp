c4e2daf30e7e450ef81a92d8a35287345c4ee530
liuli208 <liuli10@jd.com>
2016-11-22 20:32:09 +0800

    dd

--- destp-sharend-shop/src/main/java/com/destp/sharend/shop/frameworker/aop/JsonPageAspect.java
+++ destp-sharend-shop/src/main/java/com/destp/sharend/shop/frameworker/aop/JsonPageAspect.java
@@ -1,8 +1,12 @@
 package com.destp.sharend.shop.frameworker.aop;
 
+import com.destp.common.ui.web.view.RequestResult;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,10 +16,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class JsonPageAspect {
 
-    @After("@annotation(com.destp.sharend.shop.frameworker.annotation.JsonPage)")
-    public void addJsonResult(JoinPoint jp){
+    @Around("@annotation(com.destp.sharend.shop.frameworker.annotation.JsonPage)")
+    //@after("execution(* com.destp.sharend.shop.biz.home.web.RegisterController.*(..))")
+    public Object addJsonResult(ProceedingJoinPoint pjp){
+        Object result = null;
+        try {
+            result = pjp.proceed();
+            if(null!=result){
+                if(result instanceof RequestResult){
+                    ((RequestResult) result).setPagehtml("<div><p>这是后台返回来的html片段</p></div>");
+                }
+            }
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
         System.out.println("=========== JsonPageAspect ==========");
-
+        return result;
     }
 
 }
